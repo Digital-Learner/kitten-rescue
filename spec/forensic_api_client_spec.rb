1f90efae6393a739df913a94cf986f10ae7d2ef9
Digital Learner <digital.learning.journeys@gmail.com>
2019-03-14 11:39:49 +0000

    Refactor specs for #get_directions

--- spec/forensic_api_client_spec.rb
+++ spec/forensic_api_client_spec.rb
@@ -16,11 +16,15 @@ RSpec.describe KittenRescue::ForensicApiClient do
 
   describe "api calls" do
     let(:email) {'example@example.com'}
+    let(:forensics_api) { described_class.new(email: email) }
 
     context "#get_directions" do
+      let(:subject) { forensics_api.get_directions }
+
       it "returns the forensics evidence gathered" do
-        forensic_api = described_class.new(email: email)
-        expect(forensic_api.get_directions).not_to be_empty
+        expect(subject).not_to be_empty
+        expect(subject.keys).to include("directions")
+        expect(subject.values).not_to be_empty
       end
     end
   end
